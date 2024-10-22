810933c304b329546807f3326fd176cffd10ce5a
nurulamin092 <mnurulamin092@gmial.com>
2024-10-22 14:48:38 +0600

    movie cart

--- src/Headers.jsx
+++ src/Headers.jsx
@@ -2,10 +2,18 @@ import Logo from "./assets/logo.svg";
 import RingImg from "./assets/ring.svg";
 import SumImg from "./assets/icons/sun.svg";
 import ShoppingCart from "./assets/shopping-cart.svg";
+import CartDetails from "./Components/Cine/CartDetails";
+import { useState } from "react";
 const Headers = () => {
+  const [showCart, setShowCart] = useState(false);
+
+  const handleShowCartDetails = () => {
+    setShowCart(true);
+  };
   return (
     <>
       <header>
+        {showCart && <CartDetails onClose={() => setShowCart(false)} />}
         <nav className="container flex items-center justify-between space-x-10 py-6">
           <a href="index.html">
             <img src={Logo} width="139" height="26" alt="" />
@@ -32,6 +40,7 @@ const Headers = () => {
               <a
                 className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
                 href="#"
+                onClick={handleShowCartDetails}
               >
                 <img src={ShoppingCart} width="24" height="24" alt="" />
               </a>
